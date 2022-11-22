899dbba5e43abb362e34f705432739357ac9e849
MotriTN <gamerMT95@gmail.com>
2022-11-22 16:10:02 +0100

    Update Exercice1.py

--- Exercice1.py
+++ Exercice1.py
@@ -1,27 +1,17 @@
-n=int(input("Give n :")) #1234 or 4321
-l=len(str(n))
-z=1
-j=1
-a,b=False,False
-i=0
-while not j==0 and not i==(l-1):
-    i+=1
-    z=n%10
-    j=((n//10)%10)
-    print(j)
-    if z>j:
-        a=True
-        n//=10
-    elif j>z :
-        b=True
-        n//=10
-    elif z==j:
-        a=True
-        b=True
-else:
-    if (a,b)==(True,True):
-        print("This number is bad")
-    elif (a,b)==(True,False):
-        print("This number is ascending")
-    elif (a,b)==(False,True):
-        print("This number is descending")
+ch=input("Give ch : ")
+b=True
+a=True
+for i in range (len(ch)-1):
+    if (int(ch[i+1]))<(int(ch[i])) :
+        b=False
+    elif (int(ch[i+1]))>(int(ch[i])) :
+        a=False
+    elif (int(ch[i+1]))==(int(ch[i])):
+        b=False
+        a=False
+if b == (True or a==True) and len(ch)>1 :
+    print("They're sorted")
+elif len(ch)==1:
+    print("This Number can't be sorted") 
+else : 
+    print("They're not sorted")
