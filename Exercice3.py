74e49a13fb739c4b016259e5dc09df917b429760
MotriTN <gamerMT95@gmail.com>
2022-11-22 15:31:49 +0100

    Update Exercice3.py

--- Exercice3.py
+++ Exercice3.py
@@ -2,6 +2,7 @@ r = int(input("donner le nombre des notes : "))
 z = 0
 l=[]
 f=0
+b=0
 for i in range(1, r + 1):
     a = float(input(f"Donner la note n°{i} :"))
     z = z + a
@@ -10,6 +11,10 @@ for i in range(1, r + 1):
     if s>f:
         f=s
         b=a
+    if s==f and a>b:
+        f=s
+        b=a
+p=sorted(l)
 print("le moyen est:", z / r)
 print(f"le nombre le plus repeté est {b} de {f} fois ")
-print(f"la liste des nombres est : {reversed(sorted(l))} ")
+print(f"la liste des nombres est : {p[::-1]} ")
